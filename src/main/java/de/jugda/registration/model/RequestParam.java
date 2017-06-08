d6d389a981aab1353057d13e1446462d917f4e9b
Niko Köbler <niko@n-k.de>
2017-06-08 21:57:29 +0200

    fix #9 typo in charset

--- src/main/java/de/jugda/registration/model/RequestParam.java
+++ src/main/java/de/jugda/registration/model/RequestParam.java
@@ -13,8 +13,8 @@ public class RequestParam {
     public static final String GET = "GET";
     public static final String POST = "POST";
 
-    public static final Map<String, String> HEADER = Collections.singletonMap("Content-Type", "text/html; charset-utf-8");
-    public static final Map<String, String> HEADER_JSON = Collections.singletonMap("Content-Type", "application/json; charset-utf-8");
+    public static final Map<String, String> HEADER = Collections.singletonMap("Content-Type", "text/html; charset=utf-8");
+    public static final Map<String, String> HEADER_JSON = Collections.singletonMap("Content-Type", "application/json; charset=utf-8");
     public static final String ENCODING = "UTF-8";
 
     public static final String EVENT_ID = "eventId";
