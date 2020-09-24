d728f8cab0edae323dc6b8cdd94a977f91468325
Niko Köbler <niko@n-k.de>
2020-09-24 07:57:56 +0200

    extend emails with additional event data (summary, etc.)

--- src/main/java/de/jugda/registration/service/EmailService.java
+++ src/main/java/de/jugda/registration/service/EmailService.java
@@ -1,5 +1,6 @@
 package de.jugda.registration.service;
 
+import de.jugda.registration.model.Event;
 import de.jugda.registration.model.Registration;
 import io.quarkus.qute.Template;
 import software.amazon.awssdk.services.ses.SesClient;
@@ -20,31 +21,31 @@ public class EmailService {
     @Inject
     SesClient ses;
     @Inject
+    EventService eventService;
+    @Inject
     Template mail_registration;
     @Inject
     Template mail_waitlist2attendee;
 
     void sendRegistrationConfirmation(Registration registration) {
-        String eventDate = isoToGermanDateFormat(registration.getEventId());
+        Event event = eventService.getEvent(registration.eventId);
+        String subject = String.format("[JUG DA] Anmeldebestätigung für \"%s\" am %s", event.summary, event.startDate());
 
-        String subject = "[JUG DA] Anmeldebestätigung für die Veranstaltung am " + eventDate;
-
-        String mailBody = mail_registration.data("name", registration.getName())
-            .data("id", registration.getId())
-            .data("date", eventDate)
-            .data("waitlist", registration.isWaitlist())
+        String mailBody = mail_registration
+            .data("registration", registration)
+            .data("event", event)
             .render();
 
         sendEmail(registration, subject, mailBody);
     }
 
     void sendWaitlistToAttendeeConfirmation(Registration registration) {
-        String eventDate = isoToGermanDateFormat(registration.getEventId());
-        String subject = "[JUG DA] Dein Wartelisten-Eintrag für die Veranstaltung am " + eventDate;
+        Event event = eventService.getEvent(registration.eventId);
+        String subject = String.format("[JUG DA] Dein Wartelisten-Eintrag für \"%s\" am %s", event.summary, event.startDate());
 
-        String mailBody = mail_waitlist2attendee.data("name", registration.getName())
-            .data("id", registration.getId())
-            .data("date", eventDate)
+        String mailBody = mail_waitlist2attendee
+            .data("registration", registration)
+            .data("event", event)
             .render();
 
         sendEmail(registration, subject, mailBody);
@@ -70,11 +71,6 @@ public class EmailService {
             .charset(StandardCharsets.UTF_8.name())
             .data(data)
             .build();
-    }
-
-    private String isoToGermanDateFormat(String iso) {
-        String[] parts = iso.split("-");
-        return parts[2] + "." + parts[1] + "." + parts[0];
     }
 
     private static final Pattern TAGS = Pattern.compile("<.+?>");
