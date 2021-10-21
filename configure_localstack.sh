d6848155848c0fa071440ea8cf9c8ab56cd66cd2
Niko Köbler <niko@n-k.de>
2021-10-21 07:41:21 +0200

    fix localstack issue
    localstack now supports regions properly, so we have to set the proper region also in our setup script, otherwise a default region is used and this clashes with the region used in the code/config.

--- configure_localstack.sh
+++ configure_localstack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-awslocal ses verify-email-identity --email-address info@jug-da.de
-awslocal dynamodb create-table --cli-input-json file:///tmp/dynamodb-schema.json
-awslocal s3api create-bucket --bucket test
-awslocal s3 cp /tmp/events.json s3://test
-awslocal s3 cp /tmp/eventData.json s3://test
+awslocal ses verify-email-identity --email-address info@jug-da.de --region eu-central-1
+awslocal dynamodb create-table --cli-input-json file:///tmp/dynamodb-schema.json --region eu-central-1
+awslocal s3api create-bucket --bucket test --region eu-central-1
+awslocal s3 cp /tmp/events.json s3://test --region eu-central-1
+awslocal s3 cp /tmp/eventData.json s3://test --region eu-central-1
